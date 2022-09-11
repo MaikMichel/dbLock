6efd69d3d3ae48028277b0e0e4fe7b54b045d05c
Maik Michel <maik.michel78@gmail.com>
2022-09-11 20:01:47 +0200

    Add some comments to table

--- db/dblock/tables/locked_files.sql
+++ db/dblock/tables/locked_files.sql
@@ -12,9 +12,13 @@ create table locked_files (
 
 comment on table locked_files is 'Content of table';
 comment on column locked_files.lfs_id is 'PrimaryKey for table locked_files';
+comment on column locked_files.lfs_mandant is 'Mandant or Token workspace belongs to';
+comment on column locked_files.lfs_workspace is 'Project or Workspace file belongs to';
+comment on column locked_files.lfs_name is 'Name of file that is locked';
+comment on column locked_files.lfs_user is 'OS User, file was locked by';
 
 
 -- File: indexes/primaries/locked_files_lfs_id_pk.sql
 -- File: constraints/primaries/locked_files_lfs_id_pk.sql
--- File: sources/triggers/locked_files_biud.sql
 -- File: constraints/uniques/locked_files_lfs_uk.sql
+-- File: sources/triggers/locked_files_biud.sql
