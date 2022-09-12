eae12ac7cdb820e579a803c05e76ef932aff96a4
Maik Michel <maik.michel78@gmail.com>
2022-09-12 10:53:09 +0200

    Fix generation and validation of token

--- db/dblock/ddl/init/gen_token.sql
+++ db/dblock/ddl/init/gen_token.sql
@@ -6,10 +6,11 @@ set serveroutput on
 set linesize 2000
 set feedback off
 Declare
+  l_token   varchar2(200) := 'dbLock:'||to_char(sysdate, 'YYYYMMDD')||':'||sys_guid();
   l_mandant varchar2(4000);
 Begin
-  l_mandant := utl_raw.cast_to_varchar2(utl_encode.base64_encode(utl_raw.cast_to_raw('dbLock:'||to_char(sysdate, 'YYYYMMDDHH24MISS')||':'||sys_guid())));
-  dbms_output.put_line(replace(l_mandant, chr(10), ''));
+  l_mandant := utl_raw.cast_to_varchar2(utl_encode.base64_encode(utl_raw.cast_to_raw(l_token)));
+  dbms_output.put_line(l_mandant);
 End;
 /
 set feedback on
